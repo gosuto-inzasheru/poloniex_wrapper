aad56397bcdfa309cf92321437c6fb8d7c513ca8
Jorijn Smit <cloud9@jorijnsmit.com>
2016-09-06 18:49:33 +0000

    Added documentation in PHPDoc style.

--- Poloniex.php
+++ Poloniex.php
@@ -20,18 +20,28 @@ class Poloniex {
 	
 	//Public API Methods
 	
-	//Returns the ticker for all markets.
+	/**
+    * Returns the ticker for all markets.
+    * @return array
+    */
 	public function returnTicker() {
 	    return $this->callPublic('returnTicker');
 	}
 
-    //Returns the 24-hour volume for all markets, plus totals for primary currencies.
+	/**
+    * Returns the 24-hour volume for all markets, plus totals for primary currencies.
+    * @return array
+    */
 	public function return24hVolume() {
 		return $this->callPublic('return24hVolume');
 	}
 	
-	//Returns the order book for a given market, as well as a sequence number for use with the Push API and an indicator specifying whether the market is frozen. You may set currencyPair to "all" to get the order books of all markets.
-	//Set depth to limit the market depth retrieved.
+	/**
+    * Returns the order book for a given market, as well as a sequence number for use with the Push API and an indicator specifying whether the market is frozen. You may set currencyPair to "all" to get the order books of all markets. Set depth to limit the market depth retrieved.
+    * @param string $currencyPair Example: BTC_ETH
+    * @param integer $depth
+    * @return array
+    */
 	public function returnOrderBook($currencyPair = 'all', $depth = null) {
 
 	    $call = 'returnOrderBook';
@@ -41,7 +51,13 @@ class Poloniex {
 		return $this->callPublic($call);
 	}
 	
-	//Returns the past 200 trades for a given market, or up to 50,000 trades between a range specified in UNIX timestamps by the "start" and "end" GET parameters. 
+	/**
+    * Returns the past 200 trades for a given market, or up to 50,000 trades between a range specified in UNIX timestamps by the "start" and "end" GET parameters.
+    * @param string $currencyPair Example: BTC_ETH
+    * @param $start UNIX timestamp
+    * @param $end UNIX timestamp
+    * @return array
+    */
 	public function returnTradeHistory($currencyPair, $start = null, $end = null) {
 	    
 	    $call = 'returnTradeHistory';
@@ -52,7 +68,14 @@ class Poloniex {
 		return $this->callPublic($call);
 	}
 	
-	//Returns candlestick chart data. Required GET parameters are "currencyPair", "period" (candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400), "start", and "end". "Start" and "end" are given in UNIX timestamp format and used to specify the date range for the data returned.
+	/**
+    * Returns candlestick chart data.
+    * @param string $currencyPair Example: BTC_ETH
+    * @param integer $period Candlestick period in seconds; valid values are 300, 900, 1800, 7200, 14400, and 86400.
+    * @param $start UNIX timestamp
+    * @param $end UNIX timestamp
+    * @return array
+    */
 	public function returnChartData($currencyPair, $period, $start, $end) {
 	    
 	    $call = 'returnChartData';
@@ -64,12 +87,19 @@ class Poloniex {
 		return $this->callPublic($call);
 	}
 	
-	//Returns information about currencies.
+	/**
+    * Returns information about currencies.
+    * @return array
+    */
 	public function returnCurrencies() {
 	    return $this->callPublic('returnCurrencies');
 	}
 	
-	//Returns the list of loan offers and demands for a given currency, specified by the "currency" GET parameter.
+	/**
+    * Returns the list of loan offers and demands for a given currency, specified by the "currency" GET parameter.
+    * @param string $currency Example: BTC
+    * @return array
+    */
 	public function returnLoanOrders($currency) {
 	    $call = 'returnLoanOrders';
 	    $call .= '&currency='.$currency;
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# poloniex_wrapper
-Poloniex API wrapper for PHP
-
+# Poloniex API wrapper for PHP
 
 Much thanks to Compcentral for writing the initial version of this wrapper found on [Pastebin](http://pastebin.com/iuezwGRZ).
 
-It was however missing functions and Pastebin does not allow for collaborative editing...
+It was however not complete and Pastebin does not allow for collaborative editing...
 
 Any help keeping this wrapper up to date and bug free is greatly appreciated.
